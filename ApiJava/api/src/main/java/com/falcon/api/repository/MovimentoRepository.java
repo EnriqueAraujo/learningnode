3a6dd8981fa529473c717c0751abae41d5c76aa0
EnriqueAraujo <gilbertoaraujo229@gmail.com>
2020-04-22 11:13:21 -0300

    Removendo arquivos desnecessários

--- ApiJava/api/src/main/java/com/falcon/api/repository/MovimentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.falcon.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.falcon.api.datasource.model.Movimento;
-
-public interface MovimentoRepository extends JpaRepository<Movimento, Long>{
-
-}
--- ApiJava/api/src/main/java/com/falcon/api/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.falcon.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.falcon.api.datasource.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-}
